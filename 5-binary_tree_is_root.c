e3aefbae62214d4475b56d9cf9061989fe26c717
timmySpark <timmyspark1@gmail.com>
2024-01-03 23:23:58 -0800

    [feat] :- check if node is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,8 +10,8 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node != NULL && node->left != NULL &&  node->right != NULL)
-		return (1);
+	if (node == NULL || node->parent != NULL)
+		return (0);
 	return (0);
 }
 
