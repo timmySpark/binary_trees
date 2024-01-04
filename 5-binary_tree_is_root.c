cf28fd4cee746fbf0c86d887985f226a2e956a7e
timmySpark <timmyspark1@gmail.com>
2024-01-03 23:21:48 -0800

    [feat] :- check if node is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if a node is root
+ * @node: pointer to the node to check
+ *
+ * Return: 1 - if node is root
+ *         Otherwise - 0
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node != NULL && node->left != NULL &&  node->right != NULL)
+		return (1);
+	return (0);
+}
+
