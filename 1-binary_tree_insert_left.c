0ba63e163b5a8a0e82dc0b6569ed3300f7646e20
timmySpark <timmyspark1@gmail.com>
2024-01-03 22:32:46 -0800

    [feat] :- modeified header file contents as well as task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,13 +18,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new = binary_tree_node(parent, value);
+
 	if (new == NULL)
 		return (NULL);
+
 	if (parent->left != NULL)
 	{
 		new->left = parent->left;
 		parent->left->parent = new;
 	}
 	parent->left = new;
+
 	return (new);
 }
