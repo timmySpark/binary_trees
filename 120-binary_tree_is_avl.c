5965b6916c34b5868836a0629688397f4ca3b3b2
timmySpark <timmyspark1@gmail.com>
2024-01-04 18:28:05 -0800

    [feat] :- solving betty errors

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -14,15 +14,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	else
+	if (tree)
 	{
-		if (tree)
-		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		}
-		return ((l > r) ? l : r);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 	}
+	return ((l > r) ? l : r);
 }
 
 
